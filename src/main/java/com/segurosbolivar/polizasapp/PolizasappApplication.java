a921475f00ee8584899ba8434e5745eb945008fc
Kevin R <kevin.ropain@hotmail.com>
2026-03-15 22:27:11 -0500

    init: Spring Boot project with web, jpa, h2, validation

---
+++ src/main/java/com/segurosbolivar/polizasapp/PolizasappApplication.java
@@ -0,0 +1,13 @@
+package com.segurosbolivar.polizasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolizasappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolizasappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/segurosbolivar/polizasapp/PolizasappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segurosbolivar.polizasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolizasappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
